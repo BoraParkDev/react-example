40af72fdb4e5cdddf8d177e8b8906507606b2508
BoraParkDev <borapark13@gmail.com>
2023-04-16 01:23:18 +0900

    feat: timer 멈춤 기능 추가

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -5,15 +5,21 @@ const StopWatch = () => {
   const [startTime, setStartTime] = useState<number | null>(null); //시작 시간
   const [now, setNow] = useState<number | null>(null); //현재 시간
 
+  const intervalRef = useRef(1);
   const startTimer = () => {
     //타이머 시작
     setStartTime(Date.now()); //현재 시간을 시작 시간으로 설정
     setNow(Date.now()); //현재 시간을 현재 시간으로 설정
 
-    setInterval(() => {
+    stopTimer(); //타이머가 이미 실행 중이라면 멈춤
+    intervalRef.current = setInterval(() => {
       //1초마다 현재 시간을 갱신
       setNow(Date.now());
     }, 10);
+  };
+
+  const stopTimer = () => {
+    clearInterval(intervalRef.current); //타이머 멈춤
   };
 
   let secondPassed = 0;
@@ -27,7 +33,12 @@ const StopWatch = () => {
       <h1>
         시간 : <span>{secondPassed.toFixed(3)}</span>
       </h1>
-      <button onClick={startTimer}>타이머 시작</button>
+      <div
+        style={{ display: "flex", justifyContent: "space-between", width: 240 }}
+      >
+        <button onClick={startTimer}>타이머 시작</button>
+        <button onClick={stopTimer}>타이머 멈춤</button>
+      </div>
     </div>
   );
 };
