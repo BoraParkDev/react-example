f8b43c400a5091ee604d661be9c9ca0c086409ee
BoraParkDev <borapark13@gmail.com>
2023-04-16 01:04:27 +0900

    modify: 초단위 타이머 변환

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -2,18 +2,30 @@ import React, { RefObject, useRef, useState } from "react";
 import { MutableRefObject } from "react";
 
 const StopWatch = () => {
-  const [currentTime, setCurrentTime] = useState<number>(0);
+  const [startTime, setStartTime] = useState<number | null>(null); //시작 시간
+  const [now, setNow] = useState<number | null>(null); //현재 시간
 
   const startTimer = () => {
+    //타이머 시작
+    setStartTime(Date.now()); //현재 시간을 시작 시간으로 설정
+    setNow(Date.now()); //현재 시간을 현재 시간으로 설정
+
     setInterval(() => {
-      setCurrentTime((prev) => prev + 1);
-    }, 1000);
+      //1초마다 현재 시간을 갱신
+      setNow(Date.now());
+    }, 10);
   };
+
+  let secondPassed = 0;
+  if (startTime != null && now != null) {
+    //타이머가 시작되었을 때
+    secondPassed = (now - startTime) / 1000; //초단위로 변환
+  }
 
   return (
     <div>
       <h1>
-        시간 : <span>{currentTime}</span>
+        시간 : <span>{secondPassed.toFixed(3)}</span>
       </h1>
       <button onClick={startTimer}>타이머 시작</button>
     </div>
